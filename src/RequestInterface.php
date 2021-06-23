5688543aa5be344c145961046639af42dd61970f
Pascal Schnell <pascal.schnell@getmedusa.org>
2021-06-23 23:40:38 +0200

    add missing interfaces...

---
+++ src/RequestInterface.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types = 1);
+namespace Medusa\Http\Simple;
+
+/**
+ * Class Request
+ * @package medusa/http-simple
+ * @author  Pascal Schnell <pascal.schnell@getmedusa.org>
+ */
+interface RequestInterface {
+
+    /**
+     * @return UriInterface
+     */
+    public function getUri(): UriInterface;
+
+    /**
+     * Set Uri
+     * @param string|UriInterface $uri
+     * @return static
+     */
+    public function setUri(string|UriInterface $uri): static;
+
+    /**
+     * Set Uri
+     * @param string|UriInterface $uri
+     * @return static
+     */
+    public function withUri(string|UriInterface $uri): static;
+
+    /**
+     * @return string
+     */
+    public function __toString(): string;
+
+    /**
+     * @param string $method
+     * @return $this
+     */
+    public function setMethod(string $method): static;
+
+    /**
+     * @param string $method
+     * @return static
+     */
+    public function withMethod(string $method): static;
+}
---
+++ src/ResponseInterface.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types = 1);
+namespace Medusa\Http\Simple;
+
+/**
+ * Class Response
+ * @package medusa/http-simple
+ * @author  Pascal Schnell <pascal.schnell@getmedusa.org>
+ */
+interface ResponseInterface {
+
+    /**
+     * @return string
+     */
+    public function getReasonPhrase(): string;
+
+    /**
+     * @return int
+     */
+    public function getStatusCode(): int;
+
+    /**
+     * @param int         $statusCode
+     * @param string|null $phrase
+     * @return $this
+     */
+    public function setStatus(int $statusCode, ?string $phrase = null): static;
+
+    /**
+     * @param int         $statusCode
+     * @param string|null $phrase
+     * @return \Medusa\Http\Simple\Response
+     */
+    public function withStatus(int $statusCode, ?string $phrase = null): static;
+}
